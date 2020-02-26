0a0d25aab7ad59fd16ee8811e5e4f6d0098b2853
Matthew Belk <mbelk@bizspeed.com>
2020-02-26 11:58:01 -0500

    Add a unit tests project

---
+++ CPLTemplateTests/UnitTest1.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CPLTemplateTests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void ValidReceipt_WhenTranslated_ReturnsValidCPCL()
+		{
+			var receiptText = File.ReadAllText(Path.Combine(System.Environment.CurrentDirectory, "receipt.xml"));
+			var translator = new BizSpeed.CPLTemplates.XmlToCPLParser();
+
+            try
+            {
+				var cpl = translator.Translate(receiptText);
+				Assert.IsTrue(cpl.StartsWith("! U1 setvar \"device.languages\" \"line_print\""));
+			}
+            catch (Exception ex)
+            {
+				Assert.Fail($"Translation failed: {ex}");
+            }
+		}
+	}
+}
