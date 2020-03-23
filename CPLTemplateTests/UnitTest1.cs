895008f405ae0cc1a4fd0f4e15ded681a70acfde
Matthew Belk <mbelk@bizspeed.com>
2020-03-23 17:56:23 -0400

    fix: Add new receipt and test
    for grids that have no rows since this is a valid configuration
    
    References #5

--- CPLTemplateTests/UnitTest1.cs
+++ CPLTemplateTests/UnitTest1.cs
@@ -39,5 +39,21 @@ namespace CPLTemplateTests
 				Assert.Fail($"Translation failed: {ex}");
 			}
 		}
+		[TestMethod]
+		public void Valid4InchReceiptWithGridHavingNoRows_WhenTranslated_ReturnsValidCPCL()
+		{
+			var receiptText = File.ReadAllText(Path.Combine(System.Environment.CurrentDirectory, "receipt_norows.xml"));
+			var translator = new BizSpeed.CPLTemplates.XmlToCPLParser();
+
+			try
+			{
+				var cpl = translator.Translate(receiptText);
+				Assert.IsTrue(cpl.StartsWith("! U1 setvar \"device.languages\" \"line_print\""));
+			}
+			catch (Exception ex)
+			{
+				Assert.Fail($"Translation failed: {ex}");
+			}
+		}
 	}
 }
